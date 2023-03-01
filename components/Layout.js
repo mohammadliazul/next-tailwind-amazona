5b4cc489234b92ed4d062033a5e4f1f08bd046aa
mohammadliazul <liazulislam0@gmail.com>
2023-03-02 00:32:23 +0600

    Layout components added

---
+++ components/Layout.js
@@ -0,0 +1,41 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import React from 'react';
+
+const Layout = ({ title, children }) => {
+  return (
+    <>
+      <Head>
+        <title>{title ? title + ' - Amazona' : 'Amazona'}</title>
+        <meta name="description" content="Ecommerce Website" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className="flex flex-col justify-between min-h-screen">
+        <header>
+          <nav className="flex h-12 items-center justify-between shadow-md px-4">
+            <Link href="/">
+              <a className="text-lg font-bold">amazona</a>
+            </Link>
+            <div>
+              <Link href="/cart">
+                <a className="p-2">Cart</a>
+              </Link>
+              <Link href="/login">
+                <a className="p-2">Login</a>
+              </Link>
+            </div>
+          </nav>
+        </header>
+
+        <main className="container m-auto mt-4 px-4">{children}</main>
+
+        <footer className="flex h-10 justify-center items-center shadow-inner">
+          <p>Copyright &copy; 2022 Amazona</p>
+        </footer>
+      </div>
+    </>
+  );
+};
+
+export default Layout;
